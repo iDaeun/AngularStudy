ebf06e03a8bdd67148451490ec8a8ce84c8a819f
daeun <programmerdaeun@gmail.com>
2019-12-18 16:27:02 +0900

    Project reset

--- sample/src/app/app.component.ts
+++ sample/src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-  // 두 개 다른 변수 정의
-  appTitle1 = 'appTitle1';
-  appTitle2 = 'appTitle2';
 }
--- sample/src/app/app.module.ts
+++ sample/src/app/app.module.ts
@@ -1,22 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
-import { PenComponent } from './pen/pen.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
-    PenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
