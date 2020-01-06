10a4e118032741160fd240808f93b4e3d42ee295
daeun <programmerdaeun@gmail.com>
2020-01-06 11:08:41 +0900

    파일 : 초기세팅 - product-list component 추가 - top-bar component 추가 - app.component.html : top-bar 셀렉터 추가, router-outlet 셀렉터 추 - app.module.ts : RouterModule.forRoot -> ProductListComponent로 지정

--- practice-shipping/src/app/app.module.ts
+++ practice-shipping/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import {RouterModule} from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
