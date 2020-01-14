52114384be4a9f7f42db1de7a8c5baa9e67db5d6
daeun <programmerdaeun@gmail.com>
2020-01-14 11:24:56 +0900

    기능 : 양방향바인딩

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // NgModel을 제공해주는 패키지
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+// 컴포넌트 클래스에 지정하애 하는 메타데이터는 -> @Component 데코레이터에 지정
+// 어플리케이션 동작에 필요한 메타데이터는 -> @NgModule 데코레이터에 지정
 @NgModule({
+  // 사용할 컴포넌트 등록, CLI이 자동으로 등록함
   declarations: [
     AppComponent,
     HeroesComponent
   ],
+  // 어플리케이션이 동작할 떄 필요한 외부 모듈 등록
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
