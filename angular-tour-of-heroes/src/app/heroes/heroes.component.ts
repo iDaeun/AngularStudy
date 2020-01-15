93195baffa2e6a792df6ee97557c1ed3364a4d36
daeun <programmerdaeun@gmail.com>
2020-01-15 09:51:33 +0900

    기능 : 배열에 있는 데이터 표시
    - *ngFor, *ngIf 사용 :
       선택한 히어로의 상세정보를 확인

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,20 @@ import {Hero} from '../hero';
 // export해야지 다른 모듈에서 해당 컴포넌트를 import해서 사용할 수 있음
 export class HeroesComponent implements OnInit {
 
+  constructor() { }
+  // 배열 바인딩
+  heroes = HEROES;
+
   hero: Hero = {
     id : 1,
     name : 'Windstorm'
-  }
+  };
 
-  constructor() { }
+  // (click) -> onSelect() -> selectedHero에 전달받은 Hero대입
+  selectedHero: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 
   // 라이프싸이클 후킹 함수, 컴포넌트 생성한 직후에 ngOnInit호출함
   // 컴포넌트 초기화하는 로직 작성은 여기에
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+// 히어로 목록을 화면에 표시할 때 사용할 히어로 데이터
+
+import {Hero} from './hero';
+
+// HEROES배열 상수로 선언, export하여 파일 외부로 공개
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
