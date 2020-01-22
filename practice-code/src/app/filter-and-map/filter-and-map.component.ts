45a4938219c5e1069844c2b40167926c5d0a5f0a
daeun <programmerdaeun@gmail.com>
2020-01-22 15:01:24 +0900

    기능 : filter()
    참고 : https://bblog.tistory.com/300

--- practice-code/src/app/filter-and-map/filter-and-map.component.ts
+++ practice-code/src/app/filter-and-map/filter-and-map.component.ts
@@ -12,4 +12,9 @@ export class FilterAndMapComponent implements OnInit {
   ngOnInit() {
   }
 
+  filter() {
+    const arr = [1, 5, 10, 2, 7, 11];
+    const arr1 = arr.filter(element => element % 5 === 0);
+    console.log("filter ---> ", arr1); // [5, 10]
+  }
 }
