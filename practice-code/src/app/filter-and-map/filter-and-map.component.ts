e38a0f614d7eece799d915d362c03b255024b00b
daeun <programmerdaeun@gmail.com>
2020-01-24 00:03:25 +0900

    기능 : map()

--- practice-code/src/app/filter-and-map/filter-and-map.component.ts
+++ practice-code/src/app/filter-and-map/filter-and-map.component.ts
@@ -21,6 +21,12 @@ export class FilterAndMapComponent implements OnInit {
   }
 
   map() {
-
+    const arr2 = ['one', 'two', 'three', 'four'];
+    const arr3 = arr2.map(element => element.length);
+    console.log(arr3); // [3, 3, 5, 4]
+    // forEach와의 차이점 :
+    // forEach -> 상위 스코프 변수를 수정하면서 '사이드 이펙트'를 가지게 됨 = 추적하기 어려운 코드임
+    // map -> 부모 스코프 영역을 건드리지 않고 콜백 함수로만으로 목적을 달성함
+    // ** 함수형 프로그래밍 패러다임 = '순수 함수'의 연결로부터 도메인을 해결함 : Input, Output이 중요, 다른 영역 간섭 안함 (쉽고 간결한 구조 형성)
   }
 }
