8c060237a1bd8152c460773f1f8ba4feba509290
daeun <programmerdaeun@gmail.com>
2020-01-23 22:12:42 +0900

    기능 : filter()

--- practice-code/src/app/filter-and-map/filter-and-map.component.ts
+++ practice-code/src/app/filter-and-map/filter-and-map.component.ts
@@ -15,6 +15,12 @@ export class FilterAndMapComponent implements OnInit {
   filter() {
     const arr = [1, 5, 10, 2, 7, 11];
     const arr1 = arr.filter(element => element % 5 === 0);
-    console.log("filter ---> ", arr1); // [5, 10]
+    console.log("filter ---> ", arr1); // [5, 10] ,
+    // 만족하는 요소가 없다면 빈 배열 반환 (undefined가 아니라) --> 빈 배열을 반환하기 때문에 중간에 오류가 나지 않고 다음 array메소드 사용 가능!
+
+  }
+
+  map() {
+
   }
 }
