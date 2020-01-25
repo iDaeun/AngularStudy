809356b75e5f42c275d904b83c094e37c2a49cbb
daeun <programmerdaeun@gmail.com>
2020-01-25 19:07:03 +0900

    파일 : 서비스 활용 (동기 방식)

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
 
-@Injectable({
-  providedIn: 'root'
+@Injectable({ // 이 클래스가 의존성 주입 시스템에 포함되는 클래스라고 선언하는 구문
+  providedIn: 'root' // 메타데이터 값 = root, 최상위 인젝터에 등록됨 (Angular는 HeroService인스턴스 하나만 생성, 이 클래스 주입되는 모든 곳에서 같은 인스턴스 공유)
 })
 export class HeroService {
 
   constructor() { }
+
+  // 목 히어로 데이터 반환
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+// import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,9 +12,14 @@ import {HEROES} from '../mock-heroes';
 // export해야지 다른 모듈에서 해당 컴포넌트를 import해서 사용할 수 있음
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[];
+
+  // 생성자에 HeroService타입의 인자 선언
+  constructor(
+    private heroService: HeroService
+  ) { }
   // 배열 바인딩
-  heroes = HEROES;
+  // heroes = HEROES;
 
   hero: Hero = {
     id : 1,
@@ -25,10 +31,14 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes(); // 동기 방식으로 동작함
+  }
 
   // 라이프싸이클 후킹 함수, 컴포넌트 생성한 직후에 ngOnInit호출함
   // 컴포넌트 초기화하는 로직 작성은 여기에
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
