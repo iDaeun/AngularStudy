de521632db1212cd2bdba006f824b9bd47970b27
daeun <programmerdaeun@gmail.com>
2020-01-28 18:33:56 +0900

    파일 : messages 컴포넌트 생성

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 // 컴포넌트 클래스에 지정하애 하는 메타데이터는 -> @Component 데코레이터에 지정
 // 어플리케이션 동작에 필요한 메타데이터는 -> @NgModule 데코레이터에 지정
@@ -14,7 +15,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   // 어플리케이션이 동작할 떄 필요한 외부 모듈 등록
   imports: [
