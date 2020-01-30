a77b33c10139e6c648407a8aa1a3242409260183
daeun <programmerdaeun@gmail.com>
2020-01-30 10:29:57 +0900

    기능 : ChangeDetectionStrategy.OnPush
    실제 데이터가 변경되었을 경우 변경감지 하도록 수정

--- practice-code/src/app/child/child.component.ts
+++ practice-code/src/app/child/child.component.ts
@@ -1,15 +1,16 @@
-import {Component, DoCheck, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, DoCheck, Input} from '@angular/core';
 // <자식1 컴포넌트>
 // 부모 컴포넌트에서 받은 data -> 자식2 컴포넌트에 전달
 // Input -> 부모에게서 전달받은 데이터 저장
 // ngDoCheck -> 동작원리 로그 추가
 @Component({
   selector: 'app-child',
-  template: '{{data.name}} <app-childofchild [data]="data"></app-childofchild>'
+  template: '{{data.name}} <app-childofchild [data]="data"></app-childofchild>',
+  changeDetection: ChangeDetectionStrategy.OnPush // 추가 (실제 데이터가 변경되었을 때 감지하도록)
 })
 export class ChildComponent implements DoCheck {
   @Input() data: object;
-  
+
   ngDoCheck() {
     console.log('ChildComponent -- Do Check');
   }
