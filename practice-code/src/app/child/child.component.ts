6f7118c58e6cec196b0469c559a7b232dc601e3c
daeun <programmerdaeun@gmail.com>
2020-01-30 10:45:17 +0900

    기능 : ChangeDetectionRef
    OnPush -> 컴포넌트 데코레이터를 통해 설정하므로 프로그램 동작 중에 변경 x
    ==> Angular에서 지원하는 ChangeDetectionRef사용 (변경감지 세밀화)
    
    1. change() -> detach상태, 화면에 반영 안됨
    2. toggle change detection() -> attach -> 화면 갱신
    3. toggle change detection() -> detach상태, 화면에 반영 안됨

--- practice-code/src/app/child/child.component.ts
+++ practice-code/src/app/child/child.component.ts
@@ -1,15 +1,31 @@
-import {ChangeDetectionStrategy, Component, DoCheck, Input} from '@angular/core';
+import {ChangeDetectorRef, Component, DoCheck, Input} from '@angular/core';
 // <자식1 컴포넌트>
 // 부모 컴포넌트에서 받은 data -> 자식2 컴포넌트에 전달
 // Input -> 부모에게서 전달받은 데이터 저장
 // ngDoCheck -> 동작원리 로그 추가
 @Component({
   selector: 'app-child',
-  template: '{{data.name}} <app-childofchild [data]="data"></app-childofchild>',
-  changeDetection: ChangeDetectionStrategy.OnPush // 추가 (실제 데이터가 변경되었을 때 감지하도록)
+  template: '{{data.name}} ' +
+    '<button (click)="toggle()">toggle change detection</button>' +
+    '<app-childofchild [data]="data"></app-childofchild>',
 })
 export class ChildComponent implements DoCheck {
   @Input() data: object;
+
+  isDetach: boolean = true;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    cdr.detach(); // 해당 컴포넌트와 하위 컴포넌트는 변경감지 프로세스 트리에서 분리됨
+  }
+
+  toggle(){
+    this.isDetach = !this.isDetach;
+    if(this.isDetach) {
+      this.cdr.detach();
+    } else {
+      this.cdr.reattach(); // // 해당 컴포넌트와 하위 컴포넌트는 변경감지 프로세스 트리에 추가됨
+    }
+  }
 
   ngDoCheck() {
     console.log('ChildComponent -- Do Check');
