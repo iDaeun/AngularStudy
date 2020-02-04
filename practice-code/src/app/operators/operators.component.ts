f77236940bc3cbae599f2434a0e8b92de05ddf16
daeun <programmerdaeun@gmail.com>
2020-02-04 18:10:38 +0900

    기능 : Pipeable Operator 예제

--- practice-code/src/app/operators/operators.component.ts
+++ practice-code/src/app/operators/operators.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, of, pipe} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-operators',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorsComponent implements OnInit {
 
-  constructor() { }
+  public observerContext: number;
+
+  // use 'of' to change to Observable
+  // use 'pipe' to filter values
+  public observerPipeContext: Observable<number> = of(1, 2, 3, 4, 5).pipe(filter(v => v % 2 === 0));
+
+  public observer = new Observable(
+    (observer) => {
+      observer.next(Math.ceil(Math.random() * 3));
+    }
+  );
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  public pipeable() {
+    this.observerPipeContext.subscribe(
+      v => console.log(v)
+    ); // console : 2 , 4
+    // this.observer
+    //   .pipe(
+    //     filter((v: number) => !(v % 2)),
+    //     map(v => v + v),
+    //   ).subscribe(
+    //   v => this.observerContext = v
+    // );
+    // console.log('pipeable() === ', this.observerPipeContext);
   }
 
 }
